07d614cb44a20e23777b279df4b2aac34a48212e
Diptech-hub <64030987+Diptech-hub@users.noreply.github.com>
2020-10-22 13:15:57 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,34 @@
+$(".row img").on("mouseover" ,function(){
+    $(".row img").slideUp().slideDown();
+});
+
+
+$("#testimonials img").on("click" ,function(image){
+    $("#testimonials img").slideUp().slideDown();
+});
+
+
+$(".col-lg-6 button").on("click" ,function(){
+    $(".col-lg-6 button").animate({opacity: 0.25});
+    $(".col-lg-6 button").animate({opacity: 100});
+});
+
+$(".features").on("mouseover" ,function(){
+    $(".features").fadeOut().fadeIn();
+});
+
+$(".btn-block").on("click" ,function(){
+    $(".btn-block").text("LOADING...");
+    $(".btn-block").animate({opacity: 0.50});
+});
+
+
+
+
+
+
+
+
+
+
+
